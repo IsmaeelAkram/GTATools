078a84fb49e13127ce161f7b69a991be353c5943
Ismaeel <ismaeelakram01@gmail.com>
2021-04-08 14:27:16 -0400

    Finish Anti-AFK

--- MainWindow.cs
+++ MainWindow.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace GTATools
 {
@@ -11,7 +12,24 @@ namespace GTATools
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            Thread antiAfkThread = new Thread(new ThreadStart(antiAfk));
             
+            antiAfkThread.Start();
+        }
+
+        public void antiAfk()
+        {
+            while (true)
+            {
+                if (antiAfkEnabled)
+                {
+                    // TODO: Move mouse
+                    Cursor.Position = new Point(Cursor.Position.X + 300, Cursor.Position.Y);
+                }
+                Thread.Sleep(5000);
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
